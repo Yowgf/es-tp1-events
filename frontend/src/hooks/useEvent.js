543dd3a3c0f5cb415ae77b6d8ee7505a063ef29a
yowgf <alexthomasmol@gmail.com>
2021-11-16 21:41:39 -0300

    Reorganizing events table generation

--- frontend/src/hooks/useEvent.js
+++ frontend/src/hooks/useEvent.js
@@ -11,42 +11,12 @@ const useEvent = () => {
 		}
 	}
 
-	// TODO: the implementation below is just a prototype
 	const getEvent = async () => {
 		try {
-			var listEventsDiv = document.getElementById("list-events")
-			// First clean the element
-			listEventsDiv.innerHTML = ""
-
-			// Then, let's build the table from the server response
-			var res = await expressClient.getEvent().then(res => res.data)
-			var listOfEvents = res.events_list
-			var table = listEventsDiv.appendChild(document.createElement("table"))
-
-			// Create header of table
-			var headerThead = table.appendChild(document.createElement("thead"))
-			var headerRow = headerThead.appendChild(document.createElement("tr"))
-			Object.keys(listOfEvents[Object.keys(listOfEvents)[0]]).forEach(key => {
-				var header = document.createElement("th")
-				header.innerText = key
-				headerRow.appendChild(header)
-			})
-
-			// Now the body
-			var tableBody = table.appendChild(document.createElement("tbody"))
-			Object.keys(listOfEvents).forEach(key => {
-				var val = listOfEvents[key]
-				var newRow = document.createElement("tr")
-				Object.keys(val).forEach(key => {
-					var newTd = document.createElement("td")
-					newTd.innerText = val[key]
-					newRow.appendChild(newTd)
-				})
-				tableBody.appendChild(newRow)
-			})
+			return await expressClient.getEvent().then(res => res.data)
 		} catch(e) {
 			// TODO: should instead use lodash to log message -- aholmquist 2021-10-31
-			console.log("Error when getting event")
+			console.log(`Error when getting event: ${e}`)
 		}
 	}
 
