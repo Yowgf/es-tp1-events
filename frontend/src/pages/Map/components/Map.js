cb68a0876d82820c346ab7575d4606c71597c743
leonelmota <leonelmota5@gmail.com>
2022-01-07 14:31:10 -0300

    adicionando legenda e cores diferente para cada categoria

--- frontend/src/pages/Map/components/Map.js
+++ frontend/src/pages/Map/components/Map.js
@@ -7,16 +7,38 @@ import mapboxgl from "mapbox-gl";
 import '../css/Map.css'
 import '../../../errors/empty-list.css'
 
+const randomColor = () => {
+    return '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
+}
 
+// create legend with the events' categories and a random generated color for each one
+const createLegend = (eventList) => {
+    const categories = [...new Set(eventList.map(event => event.category.name))]
+    let category2color = {}
+    categories.forEach(category => {
+        category2color[category] = randomColor();
+    })
+    const legend = document.getElementById('legend');
+    Object.keys(category2color).forEach((category) => {
+        const color = category2color[category]
+        const item = document.createElement('div');
+        const key = document.createElement('span');
+        key.className = 'legend-key';
+        key.setAttribute("style", "background-color:" + color);
+        const value = document.createElement('span');
+        value.innerHTML = `${category}`;
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
+    });
+    return category2color
+
+}
 
 const addEventsLayer = (map, eventList) => {
-    const nav = new mapboxgl.NavigationControl();
-    map.addControl(nav, "top-right");
-
-    var mapDiv = document.getElementById("mapContainer")
-
     // If events list is empty, we insert a message instead
     if (Object.keys(eventList).length === 0) {
+        const mapDiv = document.getElementById("mapContainer")
         mapDiv.innerHTML =
             '<span class="empty-list-error">\
                 There are no registered events\
@@ -24,8 +46,12 @@ const addEventsLayer = (map, eventList) => {
         return
     }
 
+    const category2color = createLegend(eventList)
+
     eventList.forEach(event => {
-        new mapboxgl.Marker()
+        new mapboxgl.Marker({
+            color: category2color[event.category.name]
+        })
             .setLngLat([event.longitude, event.latitude])
             .setPopup(
                 new mapboxgl.Popup({ offset: 25 })
@@ -45,6 +71,8 @@ const createMap = () => {
         center: [-43.96, -19.91],
         zoom: 12,
     });
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, "top-right");
     return map
 }
 
@@ -67,6 +95,7 @@ const Map = () => {
     return (
         <Body>
             <div id="mapContainer" className="map-container"></div>
+            <div class='map-overlay' id='legend'></div>
         </Body>
     )
 }
