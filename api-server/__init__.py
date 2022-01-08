1f5147f1a23661f999734f538045a956b5a9ea66
yowgf <alexthomasmol@gmail.com>
2022-01-08 08:45:57 -0300

    Add .env for database config

--- api-server/__init__.py
+++ api-server/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,9 +10,16 @@ def create_app():
     from .event.storageManager.storageManager import storageManager
     from .event.storageManager.event import db
     
-    app.logger.warning('Creating app')
+    app.logger.info('Creating app')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:12345@localhost/tp_es'
+    username = os.getenv("TPES_USERNAME", "postgres")
+    password = os.getenv("TPES_PASSWORD", "12345")
+    host = os.getenv("TPES_HOST", "localhost")
+    port = os.getenv("TPES_PORT", "5432")
+    database = os.getenv("TPES_DATABASE", "tp_es")
+
+    connStr = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(username, password, host, port, database)
+    app.config['SQLALCHEMY_DATABASE_URI'] = connStr
     db.init_app(app)
     with app.app_context():
         db.create_all()
@@ -18,9 +27,6 @@ def create_app():
     app.register_blueprint(event.bp)
     app.sm = storageManager()
 
-    app.logger.warning("Done creating app")
+    app.logger.info("Done creating app")
 
     return app
-
-
-
